1d0959413ec157dde66e6b917a6a1226784304da
seedy1812 <63559352+seedy1812@users.noreply.github.com>
2024-03-05 10:15:48 +0000

    V4. DNA scroller fully working

--- spreadpoint/dna.s
+++ spreadpoint/dna.s
@@ -1,9 +1,6 @@
     seg     CODE_SEG
 
-SHIFT_BY_PIXEL equ 1
-
-
-SEEDY0: db "ANDREW SEED"
+SHIFT_BY_PIXEL equ 1            ; either scroll by letter or by pixel
 
 dna_line: db 0                  ; y coloumn to draw down
 dna_pixels_left: db 0           ; how many pixels left in letter
@@ -12,18 +9,9 @@ dna_font_ptr: dw 0              ; current letter address
 dna_y_offset: db 0
 dna_rotate: db 0                ; offset to add to table to simulate rotate
 
-SEEDY1: db "ANDREW SEED"
-
 zerobuffer: ds 64
 
-dna_writes:
-        dw dna_y_offset01,dna_y_offset02,dna_y_offset03,dna_y_offset04,dna_y_offset05,dna_y_offset06,dna_y_offset07,dna_y_offset08,dna_y_offset09,dna_y_offset10
-        dw dna_y_offset11,dna_y_offset12,dna_y_offset13,dna_y_offset14,dna_y_offset15,dna_y_offset16,dna_y_offset17,dna_y_offset18,dna_y_offset19,dna_y_offset20
-        dw dna_y_offset21,dna_y_offset22,dna_y_offset23,dna_y_offset24,dna_y_offset25,dna_y_offset26,dna_y_offset27,dna_y_offset28,dna_y_offset29,dna_y_offset30
-        dw dna_y_offset31,dna_y_offset32,dna_y_offset33,dna_y_offset34,dna_y_offset35,dna_y_offset36,dna_y_offset37,dna_y_offset38,dna_y_offset39,dna_y_offset40
-        dw dna_y_offset41,dna_y_offset42,dna_y_offset43,dna_y_offset44,dna_y_offset45,dna_y_offset46,dna_y_offset47,dna_y_offset48,dna_y_offset49,dna_y_offset50
-
-letter_lookup:
+letter_lookup:                  ; ascii to font table
         ds 32
         db  0, 1, 2, 0, 0, 0, 0, 3, 4, 5
         db  0, 0, 6, 7, 8, 0, 9,10,11,12
@@ -32,6 +20,13 @@ letter_lookup:
         db 29,30,31,32,33,34,35,36,37,38
         db 39,40,41,42,43,44,45,46,47
 
+dna_writes:
+        dw dna_y_offset01,dna_y_offset02,dna_y_offset03,dna_y_offset04,dna_y_offset05,dna_y_offset06,dna_y_offset07,dna_y_offset08,dna_y_offset09,dna_y_offset10
+        dw dna_y_offset11,dna_y_offset12,dna_y_offset13,dna_y_offset14,dna_y_offset15,dna_y_offset16,dna_y_offset17,dna_y_offset18,dna_y_offset19,dna_y_offset20
+        dw dna_y_offset21,dna_y_offset22,dna_y_offset23,dna_y_offset24,dna_y_offset25,dna_y_offset26,dna_y_offset27,dna_y_offset28,dna_y_offset29,dna_y_offset30
+        dw dna_y_offset31,dna_y_offset32,dna_y_offset33,dna_y_offset34,dna_y_offset35,dna_y_offset36,dna_y_offset37,dna_y_offset38,dna_y_offset39,dna_y_offset40
+        dw dna_y_offset41,dna_y_offset42,dna_y_offset43,dna_y_offset44,dna_y_offset45,dna_y_offset46,dna_y_offset47,dna_y_offset48,dna_y_offset49,dna_y_offset50
+
 dna_pos:    
         db  0,  6,  8, 10, 12, 13, 14, 16, 17, 18
         db 19, 20, 21, 22, 23, 24, 25, 25, 26, 27
@@ -39,19 +34,12 @@ dna_pos:
         db 36, 37, 37, 38, 39, 40, 41, 42, 43, 44
         db 45, 46, 47, 49, 50, 51, 53, 55, 57, 63
 dna_pos_end:
-        db  0,  6,  8, 10, 12, 13, 14, 16, 17, 18
-        db 19, 20, 21, 22, 23, 24, 25, 25, 26, 27
-        db 28, 29, 30, 31, 31, 32, 32, 33, 34, 35
-        db 36, 37, 37, 38, 39, 40, 41, 42, 43, 44
-        db 45, 46, 47, 49, 50, 51, 53, 55, 57, 63
 
 
 dma_palette:
         incbin "sa.nxp"        
 
-        align 256
 dna_scroll_start:
-        db ".0123456789",0
         db "YO, SUCKERS!!!  THIS IS THE SPREADPOINT DEMO!   ALL CODING WAS DONE BY THE "
         db "CAREBEARS,  ALL GRAPHIXX WAS DESIGNED BY THE CAREBEARS AND THE MUZEXX WAS ALSO "
         db "'EINGEPLUPPED' BY THE CAREBEARS!  WE SAW THE ORIGINAL SPREADPOINT DEMO, BY "
@@ -74,6 +62,7 @@ dna_init:
     srl a
     ld (layer_2_back),a
     nextreg LAYER2_SHADOW_BANK,a
+
 if 1
     Call Layer2cls
 else
@@ -97,23 +86,10 @@ endif
     ret
 
 Layer2cls:
-
-    ld a,%00001111
-    ld bc, $123b   
-	out (c), a
-    ld a,2
-    call cls
-
-    ld a,%01001111      ; first 16
-    ld bc, $123b   
-	out (c), a
-    ld a,3
-    call cls
-
     ld a,%10001111      ; first 16
     ld bc, $123b   
 	out (c), a
-    ld a,1
+    ld a,0
     call cls
 
     ret
@@ -126,44 +102,42 @@ cls:
     ldir
     ret
 
-
-
-
 dna_copper:
-    ld ix,dna_writes
-    ld c,0
-    ld a,(dna_rotate)
-    ld de,dna_pos
-    add de,a
- ;   inc a
+    ld ix,dna_writes        ; pointer to address to se the scrollery in coppet
+    ld c,0                  ; y in the scroller 0 ->N
+    ld a,(dna_rotate)       ; step round the scroller Y
+    inc a
     and 63
     ld (dna_rotate),a
+    ld iyl,a                ; store this here
     
-    ld b, dna_pos_end - dna_pos
+    ld b, dna_pos_end - dna_pos ; for all the lines in the scroller
+    ld de,dna_pos               ; these are the default Y values
 .loop:
-    ld l,(ix+0)
+    ld a,(de)                   ; read the Y line to draw - default
+    add a,iyl                   ; add on the rotation
+    and 63                      ; only 64 lines in image  0->63 
+    sub c                       ; find delta offset to shift Layer 2 by
+
+    jr nc,.flip                 ; if negative we have to adjust as the values are 0 ->192 not 0->255
+    sub 64
+.flip:
+    add 3                       ; shift scroller down screen to look like original
+
+    ld l,(ix+0)                 ; read the copper address we will set the laer 2 dY
     ld h,(ix+1)
 
-    ld a,(de)
-    sub c
+    ld (hl),a                   ; write it
 
-   add 3
-
-    ld (hl),a
-
-    inc de
+    inc de                     ; point to the next line data
     inc ix
     inc ix
     inc c
-    djnz .loop
+    djnz .loop                 ; do all the lines
     ret
 
 
 dna_test:
-   nextreg $17,0
-   nextreg $16,0
-
-
     ld a,(dna_pixels_left)
     or a
     jr nz,.same_letter
@@ -237,11 +211,11 @@ endif
 
     // needs spliiting into parts as can wrap the buffer
 
-    // ->|XXXXXXX........|<- if letter at top then fill 1 span for letter  and 1 blank span
+    // we clear the line out first - only 64*2 bytes
 
-    // ->|..XXXXXXX......|<- if letter not at past bottom  then fill 1 span for letter  and 2 blank span
+    // ->|..XXXXXXX......|<- if letter not at past bottom  then fill 1 span for letter
 
-    // ->|XXXXX........XX|<- if letter goes past bottom  then fill 2 spans for letter  and 1 blank span
+    // ->|XXXXX........XX|<- if letter goes past bottom by clearing bit 6 ( and 63) then it wraps
 
 
     // clear the vertical line
@@ -283,7 +257,6 @@ endif
     inc e
     pop bc
 
- ;   ld d,a
     djnz .loop
 
 if  SHIFT_BY_PIXEL
@@ -292,8 +265,6 @@ if  SHIFT_BY_PIXEL
     and $3f
     ld (dna_y_offset),a
 endif
-
-
 
     ld (dna_font_ptr),hl
 
